05b6064c3573af80be8de3948eee64e06ff4be5a
nasr7322 <120938183+nasr7322@users.noreply.github.com>
2023-12-03 11:52:34 +0200

    Front end (#3)
    * initializing vue project
    
    * front end canvas and panel
    
    * final touches to the front-end look

---
+++ front-end/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <PaintApp/>
+</template>
+
+<script>
+import PaintApp from './components/PaintApp.vue';
+
+export default {
+    name: "App",
+    components: { PaintApp }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+}
+</style>
